a1de98a6dca08042e64756ad4e5d2cb5698206d5
Cumayevomr <cumayevomr8@gmail.com>
2025-05-09 14:10:00 +0400

    monthname generating

--- assets/js/module.js
+++ assets/js/module.js
@@ -4,3 +4,45 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+"use strict";
+
+import { data } from "autoprefixer";
+
+export const weekDayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthNames = [
+    "Jan",
+    "Feb",
+    "Fab",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+/**
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Date String. formate: "Sunday 10, Jan"
+ */
+export const getDate = function (dateUnix, timezone) {
+    const date = new Date((dateUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames[date.getUTCMonth()];
+
+    return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+}
