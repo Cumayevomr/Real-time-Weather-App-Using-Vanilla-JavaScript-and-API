ff2f59fa6136d906b6001e2a62711e99f4b59b0b
Cumayevomr <cumayevomr8@gmail.com>
2025-05-09 11:59:59 +0400

    api_key generating

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,30 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key = "a40b3e71346fa67db5abfaead31822bc";
+
+/**
+ * 
+ * @param {string} URL ApI url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    }
+}
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 /**
  * @license MIT
- * @fileoverview
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
