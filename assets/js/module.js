a6edc511cd162777086aa16eb8922cc53a51aa0c
Cumayevomr <cumayevomr8@gmail.com>
2025-05-09 15:54:22 +0400

    getHours

--- assets/js/module.js
+++ assets/js/module.js
@@ -46,3 +46,40 @@ export const getDate = function (dateUnix, timezone) {
 
     return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
 }
+
+/**
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time string. formate: "HH:MM AM/PM"
+ */
+export const getTime = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date .getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12}:${minutes} ${period}`;
+}
+
+/**
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time string. formate: "HH AM/PM"
+ */
+export const getHours = function (timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date .getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+/**
+ * @param {*number} mps Metter per seconds
+ * @returns {number} Kilometer per hours
+ */
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
+}
+
